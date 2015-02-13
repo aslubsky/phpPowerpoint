58779940eafde7f0fe4d732deff1e4a1c097fb38
Alex Slubsky <aslubsky@gmail.com>
2015-02-13 16:45:12 +0200

    init

--- parse.php
+++ parse.php
@@ -18,7 +18,35 @@ $powerpoint->buildAll();
 $l = $powerpoint->getNumberOfSlides();
 for($i=0; $i<$l; $i++) {
     //print_r($powerpoint->getSlide(0)->getHTML());
-    echo '<section data-v="1" data-h="'.($i+1).'">'.$powerpoint->getSlide($i)->getHTML() .'</section>'."\n";
+	$slide = $powerpoint->getSlide($i);
+	$slideCont = [];
+	$slideCont []= '<section data-v="1" data-h="'.($i+1).'">';
+	$prevParagraph = null;
+	$p = 1;
+	foreach($slide->paragraphs as $paragraph) {
+		print_r($slide);exit;
+		$markup = $paragraph->getHTML($prevParagraph);
+		$id = uniqid();
+		$objCont = [];
+		$objCont []= '<div slides-editor-object class="object move" data-id="'.$id.'" tabindex="'.$p.'" style="">';
+		$objCont []= '<div class="object-expand-point tl"></div>';
+		$objCont []= '<div class="object-expand-point ml"></div>';
+		$objCont []= '<div class="object-expand-point bl"></div>';
+		$objCont []= '<div class="object-expand-point tc"></div>';
+		$objCont []= '<div class="object-expand-point bc"></div>';
+		$objCont []= '<div class="object-expand-point tr"></div>';
+		$objCont []= '<div class="object-expand-point mr"></div>';
+		$objCont []= '<div class="object-expand-point br"></div>';
+		$objCont []= '<editor contenteditable="false" ng-model="text" class="content" style="padding: 0px;">'.$markup.'</editor>';
+		$objCont []= '</div>';
+		$slideCont []= implode('', $objCont);
+
+		$prevParagraph = $paragraph;
+		$p++;
+	}
+	$slideCont []= '</section>';
+//	echo implode('', $slideCont)."\n";
+	exit;
 }
 
 //echo $powerpoint->getHTML();
